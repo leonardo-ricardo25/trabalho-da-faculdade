5ca37e6071b5df02e8594d4de2be95a0d2049d09
leonardo-ricardo25 <leonardo.dev2026@gmail.com>
2025-06-22 19:22:27 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("saudacao");
+const saudacao = document.getElementById("mensagem-saudacao");
+
+botao.addEventListener("click", function () {
+  saudacao.innerHTML = "Seja Bem-Vindo ao meu Trabalho";
+});
